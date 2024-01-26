4687b2acd5a1a507c6a8c819c60cb67a57e5f8de
Verde <carlos-verde@outlook.com>
2024-01-26 16:31:06 -0300

    build: :heavy_plus_sign: Add styled components, theme colors and custom font

--- App.tsx
+++ App.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components/native';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
+
+import theme from './src/global/styles/theme';
 import { Dashboard } from './src/screens/Dashboard';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular, 
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return SplashScreen;
+  }
+
   return (
+    <ThemeProvider theme={theme}>
       <Dashboard />
+    </ThemeProvider>
   );
 }
--- src/screens/Dashboard/style.ts
+++ src/screens/Dashboard/style.ts
@@ -3,11 +3,12 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
     flex: 1;
     align-items: center;
+    background-color: ${({theme})=>theme.colors.background};
     justify-content: center;
     `;
 
 export const Title = styled.Text`
+    font-family: ${({theme}) => theme.fonts.bold};
     font-size: 24px;
-    color: purple;
-    font-weight: bold;
+    color: ${({theme}) => theme.colors.title};
     `;
